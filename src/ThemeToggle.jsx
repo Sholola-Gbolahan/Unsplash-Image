a4f441ff135b1e73c9214e3072922f2d728d3a0e
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-28 05:12:13 +0100

    How to add and remove class to body in Darkmode

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,11 +1,21 @@
 import React, { useContext } from "react"
 import { useGlobalContext } from "./context"
 import { FaMoon } from "react-icons/fa"
-import { IoSunnyOutline } from "react-icons/io5";
+import { IoSunnyOutline } from "react-icons/io5"
 
 const ThemeToggle = () => {
   const { isDarkTheme, toggleDarkTheme } = useGlobalContext()
 
-  return <button onClick={toggleDarkTheme}>{isDarkTheme ? <FaMoon/> : <IoSunnyOutline/>} </button>
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <FaMoon className="toggle-icon" />
+        ) : (
+          <IoSunnyOutline className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  )
 }
 export default ThemeToggle
--- src/context.jsx
+++ src/context.jsx
@@ -8,7 +8,17 @@ const AppContext = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   const toggleDarkTheme = () => {
-    setIsDarkTheme(!isDarkTheme)
+    // 1 storing the state valuein a variable to be used in multiple places
+    const newDarkTheme = !isDarkTheme
+    setIsDarkTheme(newDarkTheme)
+    //2.  This line selects the body element of the current document using the document.querySelector() method, which returns the first element that matches the specified selector. In this case, it is selecting the body element.
+    const body = document.querySelector("body")
+
+    //3. This line toggles the dark-theme class of the body element. The classList property is a read-only list that contains the classes of an element. The toggle() method adds a class to the element if it does not have it, or removes it if it does. In this case, it adds the dark-theme class if isDarkTheme is true, and removes it if isDarkTheme is false.
+    body.classList.toggle("dark-theme", newDarkTheme)
+
+    //4. displaying to see the class toggle in console
+    console.log(body)
   }
 
   return (
