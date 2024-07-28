bfcd7808ba25771d99068e4e2f1df88d61a3509a
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-28 23:39:38 +0100

    How to setup React Query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import AppContext from "./context"
+//1. import from react query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+// 2. setup an instance
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppContext>
-    <App />
-  </AppContext>
+  //3. Wrapped entire application in the QueryClientProvider
+  // 4. PAssedthe queryClient as a props
+  <QueryClientProvider client={queryClient}>
+    <AppContext>
+      <App />
+    </AppContext>
+  </QueryClientProvider>
 )
