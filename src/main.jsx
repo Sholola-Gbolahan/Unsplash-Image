2e6ee241621383f9bf94bab569defe5f3c6c70c9
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-31 03:10:10 +0100

    How to setup React Query Dev tools for use

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,18 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import AppContext from "./context"
-//1. import from react query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-// 2. setup an instance
+//1. import react query dev tools for use
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  //3. Wrapped entire application in the QueryClientProvider
-  // 4. PAssedthe queryClient as a props
-
   <AppContext>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* 2. Applying it in my code*/}
+      {/* 3. this give bunch of usefull info about our query */}
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </AppContext>
 )
