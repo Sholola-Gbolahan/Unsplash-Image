0f5561ec3511cf2ee2039759b0f273d54479f971
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-08-01 18:00:34 +0100

    How to ~ store the initial mode selection in localStorage, check and see if initial mode selection is  darkMode from localStorage

--- src/context.jsx
+++ src/context.jsx
@@ -4,26 +4,32 @@ const GlobalContext = createContext()
 
 export const useGlobalContext = () => useContext(GlobalContext)
 
-const AppContext = ({ children }) => {
-  //A function to check if the system is set to dark Mode by default
-  const getInitialDarkMode = () => {
-    const preferDarkMode = window.matchMedia(
-      "( prefers-color-scheme:dark)"
-    ).matches
-    console.log(preferDarkMode)
+//A function to check if the system is set to dark Mode by default
+const getInitialDarkMode = () => {
+  const preferDarkMode = window.matchMedia(
+    "( prefers-color-scheme:dark)"
+  ).matches
+  //checking if initial value in localStorage is set to dark Mode
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true"
 
-    return preferDarkMode
-  }
+  return storedDarkMode || preferDarkMode
+}
+
+const AppContext = ({ children }) => {
   // setting the return value to State
   const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode)
   const [searchTerm, setSearchTerm] = useState("cat")
 
   const toggleDarkTheme = () => {
-    setIsDarkTheme(!isDarkTheme)
+    const newDarkTheme = !isDarkTheme
+    setIsDarkTheme(newDarkTheme)
+    //Storing the value of selected theme to local storage
+    localStorage.setItem("darkTheme", newDarkTheme)
   }
   useEffect(() => {
     document.body.classList.toggle("dark-theme", isDarkTheme)
   }, [isDarkTheme])
+
   return (
     <GlobalContext.Provider
       value={{ toggleDarkTheme, isDarkTheme, setSearchTerm, searchTerm }}
